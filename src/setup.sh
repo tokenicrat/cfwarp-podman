230d815685c066c3e840f1ef6c422ad86f6ea3ef
Tokenicrat <hi@bytoken.link>
2025-08-05 21:11:04 +0800

    Fix (again) multi-arch build

--- src/setup.sh
+++ src/setup.sh
@@ -2,8 +2,14 @@ set -e
 
 apk add --no-cache curl
 
-curl -sSL "https://github.com/ViRb3/wgcf/releases/download/v${WGCF_VERSION}/wgcf_${WGCF_VERSION}_linux_amd64" -o wgcf
-curl -sSL "https://github.com/whyvl/wireproxy/releases/download/v${WIREPROXY_VERSION}/wireproxy_linux_amd64.tar.gz" -o wireproxy.tar.gz
+case "$(uname -m)" in
+    x86_64) ARCH=amd64 ;;
+    aarch64) ARCH=arm64 ;;
+    *) echo "Unsupported architecture: $(uname -m)" && exit 1 ;;
+esac
+
+curl -sSL "https://github.com/ViRb3/wgcf/releases/download/v${WGCF_VERSION}/wgcf_${WGCF_VERSION}_linux_${ARCH}" -o wgcf
+curl -sSL "https://github.com/whyvl/wireproxy/releases/download/v${WIREPROXY_VERSION}/wireproxy_linux_${ARCH}.tar.gz" -o wireproxy.tar.gz
 tar -xvzf wireproxy.tar.gz
 rm wireproxy.tar.gz
 chmod +x wgcf wireproxy
